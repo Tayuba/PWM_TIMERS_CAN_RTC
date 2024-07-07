134e83c4dd43dec0ed24a4f7dfc8830c0e1fe5df
Tayuba <ayubamonnietahiru@gmail.com>
2024-07-07 12:16:11 +0200

    Timers

---
+++ TIME_BASE_100Ms_IT/Core/Inc/app.h
@@ -0,0 +1,19 @@
+/*
+ * appIT.h
+ *
+ *  Created on: Jul 7, 2024
+ *      Author: Ayuba Tahiru
+ */
+
+#ifndef INC_APP_H_
+#define INC_APP_H_
+
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdio.h>
+
+#define HCLK_50MHZ 50
+#define HCLK_84MHZ 84
+#define HCLK_120MHZ 120
+
+#endif /* INC_APP_H_ */
---
+++ TIME_BASE_100Ms_IT/Core/Inc/interrupt.h
@@ -0,0 +1,18 @@
+/*
+ * interruptIT.h
+ *
+ *  Created on: Jul 7, 2024
+ *      Author: Ayuba Tahiru
+ */
+
+#ifndef INC_INTERRUPT_H_
+#define INC_INTERRUPT_H_
+
+
+//#include "stm32f4xx_hal.h"
+#include <app.h>
+
+
+
+
+#endif /* INC_INTERRUPT_H_ */
